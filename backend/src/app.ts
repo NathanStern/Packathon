dcc8486e048ee4a1d47af3e79d4a2fd22ef7c31d
Nathan Stern <nathan@houseofstern.com>
2023-01-21 12:49:42 -0500

    add dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const port = 3000;
+
+console.log(process.env.sqlusername);
 
 interface TestInterface {
     name: string;
