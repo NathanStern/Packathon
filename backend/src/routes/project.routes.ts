28eef60b999ae4077f9b8a456e7b8489c5e5bbd3
Nathan Stern <nathan@houseofstern.com>
2023-01-21 17:23:37 -0500

    initial project routes

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,14 +1,13 @@
 import { Express, Request, Response, Router } from 'express';
-import { get_user_profile } from '../controllers/user.controller';
 
-export const user_routes = (app: Express) => {
+export const project_routes = (app: Express) => {
     const router = Router();
 
-    router.get('/profile', get_user_profile);
+    // router.get('/list');
 
     router.use('/', (req: Request, res: Response) => {
-        res.send({message: "User Routes"});
+        res.send({message: "Project Routes"});
     });
 
-    app.use('/users', router);
+    app.use('/project', router);
 }
